67140fcbdb2aea840a8469ae572fb04e428c3b8b
Rivan Chandra <rivan@genesisdigital.asia>
2023-07-16 02:30:53 +0700

    add form and tab

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { TabView, TabPanel } from 'primereact/tabview';
+import { InputText } from 'primereact/inputtext';
 
 export default function Home() {
   return (
     <div class="grid">
       <div class="col">
-        <div class="text-center p-3 border-round-sm bg-primary font-bold ">left</div>
+        <div className="card">
+          <TabView>
+            <TabPanel header="Header I">
+              <div className="flex flex-column gap-2">
+                <label htmlFor="username">Username</label>
+                <InputText id="username" aria-describedby="username-help" />
+            </div>
+            </TabPanel>
+            <TabPanel header="Header II">
+              <p className="m-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+              </p>
+            </TabPanel>
+            <TabPanel header="Header III">
+              <p className="m-0">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
+                culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+              </p>
+            </TabPanel>
+          </TabView>
+        </div>
       </div>
       <div class="col">
         <div class="text-center p-3 border-round-sm bg-primary font-bold ">right</div>
