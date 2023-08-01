05025cfe15b66ab922f24cf8c30f96e28f0062f9
Rivan Chandra <rivan@genesisdigital.asia>
2023-08-01 20:34:02 +0700

    Add author and update favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://unpkg.com/primeflex@latest/primeflex.css" /> 
+        <link rel="stylesheet" href="https://unpkg.com/primeflex@latest/primeflex.css" />
+        <title>Github Profile Helper</title>
       </Head>
       <body>
         <Main />
+        <a href="https://github.com/rivanchandra" target="_blank"><img src="profile.png" style={{position:'fixed',bottom:'5px',right:'5px'}} /></a>
         <NextScript />
       </body>
     </Html>
